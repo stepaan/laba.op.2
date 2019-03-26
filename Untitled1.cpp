fa17d44f5a300829f9e90d4e03e1a627a442b5e3
stepaan <47590281+stepaan@users.noreply.github.com>
2019-03-26 21:40:20 +0200

    read and plus

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 
 using namespace std;
-
+struct comand
+{
+	string name;
+	int rez;
+	comand(void)
+	{
+		rez=0;
+	}
+}
 int main()
+{
+	int a;
+	ifstream input("premier_league.csv");
+	input >> a;
+	input.ignore(1);
+	command *fil=new command[a];
+	string d;
+	for(int i=0;i<a;i++)
+	{
+		int k,r;
+		getline(input,fil[i].name,',');
+		for(int j=0;j<10;j++)
+		{
+			getline(input,d,':');
+			k=stoi(d);
+			getline(input,d,',');
+			r=stoi(d);
+			if(k>r)
+			fil[i].rez=+3;
+			else if(k==r)
+			fil[i].rez=+1;
+		}
+	}
+}
