30c125347b6e1ab154c3b354fe0b66799b7e78ea
stepaan <47590281+stepaan@users.noreply.github.com>
2019-03-26 22:53:22 +0200

    final v.0.0.1

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,63 +1,73 @@
 #include<iostream>
 #include<string>
 #include<fstream>
+using namespace std;
 struct command
 {
 	string name;
-	int rez; //rezultat
-};
-using namespace std;
-<<<<<<< HEAD
-struct comand
-{
-	string name;
 	int rez;
-	comand(void)
+	command(void)
 	{
-		rez=0;
-	}
-}
+		rez = 0;
+	};
+};
+void sort(command[]);
+void newfile(command[]);
+void read(command[],int);
 int main()
 {
 	int a;
 	ifstream input("premier_league.csv");
 	input >> a;
 	input.ignore(1);
-	command *fil=new command[a];
+	command *fil = new command[a];
+	read(fil, a);
+	sort(fil);
+	newfile(fil);
+	input.close();
+}
+void read(command fil[],int a)
+{
+	ifstream input("premier_league.csv");
+	input.ignore(3);
 	string d;
-	for(int i=0;i<a;i++)
+	for (int i = 0; i < a; i++)
 	{
-		int k,r;
-		getline(input,fil[i].name,',');
-		for(int j=0;j<10;j++)
+		int k, r;
+		getline(input, fil[i].name, ',');
+		for (int j = 0; j < 9; j++)
 		{
-			getline(input,d,':');
-			k=stoi(d);
-			getline(input,d,',');
-			r=stoi(d);
-			if(k>r)
-			fil[i].rez=+3;
-			else if(k==r)
-			fil[i].rez=+1;
+			getline(input, d, ':');
+			k = stoi(d);
+			getline(input, d, ',');
+			r = stoi(d);
+			if (k > r)
+				fil[i].rez = fil[i].rez + 3;
+			else if (k == r)
+				fil[i].rez = fil[i].rez + 1;
 		}
+		getline(input, d, ':');
+		k = stoi(d);
+		getline(input, d, '\n');
+		r = stoi(d);
+		if (k > r)
+			fil[i].rez = fil[i].rez + 3;
+		else if (k == r)
+			fil[i].rez = fil[i].rez + 1;
 	}
 }
-=======
-void sort(command );
-void newfile(command );
+
 void sort(command fill[])
 {
-	int a, g=1;
-	while(g>0)
+	int g = 1;
+	while (g > 0)
 	{
 		g = 0;
 		for (int i = 0; i < 19; i++)
 		{
-			if (fill[i].rez > fill[i+1].rez)
+			if (fill[i].rez < fill[i + 1].rez)
 			{
-				a = fill[i].rez;
-				fill[i].rez = fill[i + 1].rez;
-				fill[i + 1].rez = a;
+				swap(fill[i], fill[i + 1]);
 				g++;
 			}
 		}
@@ -73,4 +83,4 @@ void newfile(command fill[])
 	output << fill[19].name << "," << fill[19].rez;
 	output.close();
 }
->>>>>>> mykImp
+
